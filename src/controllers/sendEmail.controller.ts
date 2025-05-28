56d67a19facde403458fb4869c0ed8fb3ce0b9c3
sidpd51 <sidpd510@gmail.com>
2025-05-28 23:22:35 +0530

    feat: add logging for email queue processing and error handling in sendEmailHandler

--- src/controllers/sendEmail.controller.ts
+++ src/controllers/sendEmail.controller.ts
@@ -2,10 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import { sendEmailService } from "../service/sendEmail.service";
 import { StatusCodes } from "http-status-codes";
 import { InternalServerError } from "../utils/errors/app.error";
+import { logger } from "../config/logger.config";
 
 export const sendEmailHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
         await sendEmailService(req.body);
+        logger.info("Successfully added email payload to email-queue!");
         res.status(StatusCodes.ACCEPTED).json({
             message: "We've successfully added your email to the queue. It will be processed shortly.",
             success: true,
@@ -13,6 +15,7 @@ export const sendEmailHandler = async (req: Request, res: Response, next: NextFu
         });
     } catch (error) {
         if (error instanceof InternalServerError) {
+            logger.error(`Error in sendEmailHandler, ${error.message}`);
             res.status(error.statusCode).json({
                 message: error.message,
                 success: false,
