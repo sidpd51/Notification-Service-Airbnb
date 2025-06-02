4ff24dbd98b540996fc8c5d496871f5e2008f674
sidpd51 <sidpd510@gmail.com>
2025-06-02 18:48:24 +0530

    feat: remove SMS handling functionality and related components

--- src/queues/email.queue.ts
+++
@@ -1,5 +0,0 @@
-import { Queue } from "bullmq";
-import { connection } from "../config";
-
-
-export const emailQueue = new Queue("email-queue", { connection });
--- src/queues/sms.queue.ts
+++
@@ -1,5 +0,0 @@
-import { Queue } from "bullmq";
-import { connection } from "../config";
-
-
-export const smsQueue = new Queue("sms-queue", { connection });
