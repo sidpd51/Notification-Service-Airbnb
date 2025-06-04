cf221e0ac9d08582338415e401fbfcdebd250ba4
siddharthpd <sidpd510@gmail.com>
2025-06-04 14:37:17 +0530

    feat: add request body validation for email sending endpoint

--- src/routers/v1/index.ts
+++ src/routers/v1/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { sendEmailHandler } from '../../controllers/sendEmail.controller';
+import { validateRequetBody } from '../../validators';
+import { emailPayloadSchema } from '../../validators/email.validator';
+
 
 const router = express.Router();
 
 
-router.post('/email', sendEmailHandler);
+router.post('/email', validateRequetBody(emailPayloadSchema), sendEmailHandler);
 
 export default router;
--- src/validators/email.validator.ts
+++ src/validators/email.validator.ts
@@ -1,29 +1,15 @@
 import { z } from "zod";
 
-const templateTypeEnum = z.enum(["WELCOME", "BOOKING"]); // extend as needed
-
-const attachmentSchema = z.object({
-    filename: z.string().min(1, "Filename is required"),
-    url: z.string().url("Attachment URL must be a valid URL")
-});
-
-const userSchema = z.object({
-    name: z.string().min(1, "name can't be empty"),
-    email: z.string().email()
-});
+const templateTypeEnum = z.enum(["welcome", "booking"]);
 
 
-const emailPayloadSchema = z.object({
-    to: z.array(userSchema),
+const paramsSchema = z.record(z.string(), z.any());
+
+export const emailPayloadSchema = z.object({
+    to: z.string().email("Invalid recipient email"), 
     cc: z.array(z.string().email()).optional(),
     bcc: z.array(z.string().email()).optional(),
     subject: z.string().min(1, "Subject is required"),
-    orderId: z.string()
-        .min(1, "Order ID cannot be empty")
-        .regex(/^[a-zA-Z0-9_-]+$/, "Order ID must be alphanumeric with optional dashes or underscores").optional(),
-    templateType: templateTypeEnum,
-    attachments: z.array(attachmentSchema).optional()
+    template: templateTypeEnum, 
+    params: paramsSchema.optional(), // Accept any key-value pairs
 });
-
-export type EmailPayload = z.infer<typeof emailPayloadSchema>;
-export type UserSchemaPaylod = z.infer<typeof userSchema>;
